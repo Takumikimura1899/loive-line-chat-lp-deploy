b396949260757c76328ebefbac4cf6f317fdb4ae
fukata-bc <k.fukata@blue-crystal.co.jp>
2025-02-07 10:21:25 +0900

    add: Q2チャット部分の動作作成

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+// Button
+const chatRefreshButtonElement = document.querySelector('#chat-refresh-button');
+const chatDietButtonElement = document.querySelector('#chat-diet-button');
+
+// ChatList
+const chatListRefreshElement = document.querySelector('#chat-list-refresh');
+const chatListDietElement = document.querySelector('#chat-list-diet');
+
+// ButtonEvent
+chatRefreshButtonElement.addEventListener('click', () => {
+  chatListRefreshElement.classList.remove('hidden');
+});
+
+chatDietButtonElement.addEventListener('click', () => {
+  chatListDietElement.classList.remove('hidden');
+});
+
 const programCardSlideElement = document.querySelector('.program-card-slide');
 const programLessonCardSlideElement = document.querySelector('.program-lesson-card-slide');
 
